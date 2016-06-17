e0e5128582c4ef7c6ae558d22f778b856d3761b6
Hamza Kaya <hamza@cortexpowered.com>
2016-06-17 13:44:42 -0400

    Simple bash script to build zip files & upload to s3

---
+++ pack.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+set -o nounset
+
+BUILD=./build
+S3BUCKET=s3://cortex-test-assets/animation/
+
+rm -rf ${BUILD}
+npm run build
+
+for app in opacity rotate scale skew translate
+do
+    pushd ${BUILD}/${app}
+    zip -r ${app}.zip *
+    aws s3 cp ${app}.zip ${S3BUCKET} --acl public-read
+    popd
+done
